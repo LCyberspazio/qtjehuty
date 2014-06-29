8af1589714cf502f3715573f1b9a4ea7e5789082
LCyberspazio <idmgiovanni@libero.it>
2014-06-30 00:37:46 +0200

    Added salt to the message v0.2. This version is uncompatible with 0.1

--- QTJehuty/mainwindow.cpp
+++ QTJehuty/mainwindow.cpp
@@ -11,17 +11,53 @@
 #include "ui_mainwindow.h"
 #include <QClipboard>
 #include <QMessageBox>
+#include <time.h>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
+    srand(time(NULL));
     ui->setupUi(this);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+/*
+ * Generates a random string of charc-number of characters
+ * if block == 1 then the string will be enclosed by [ ]
+ * if block == 2 then the string will begin with \n ;)
+ */
+QString* randomString(int charc, int block)
+{
+    int i=0;
+    QString *str=new QString();
+
+    if(charc<=0)return NULL;
+
+    if(block==0)
+    {
+        for(i=0; i<charc; i++)
+            str->append((char)((rand()%60)+64));
+    }
+    else if(block==1)
+    {
+        str->append('è');
+        for(i=0; i<charc; i++)
+            str->append((char)((rand()%60)+64));
+        str->append(']');
+    }
+    else if(block==2)
+    {
+        str->append('\n');
+        for(i=0; i<charc; i++)
+            str->append((char)((rand()%60)+64));
+    }
+
+    return str;
 }
 
 void MainWindow::on_encryptbutton_clicked()
@@ -62,8 +98,17 @@ void MainWindow::on_encryptbutton_clicked()
 
 
 
+    /* Adding the salt to the string both beginning and ending */
+    QString *rndtext=randomString((int)(rand()%5)+1,1);
+    QString elabtext;
+    elabtext=*rndtext+ui->cleartextline->text();
+    delete rndtext;
+    rndtext=randomString((int)(rand()%42)+15,2);
+    elabtext+=*rndtext;
+    delete rndtext;
+
     memset(plaintext,0,1024);
-    strcpy(plaintext, ui->cleartextline->text().toStdString().c_str());
+    strcpy(plaintext, /*ui->cleartextline->text()*/elabtext.toStdString().c_str());
     ui->ciphertextline->clear();
 
     p=plaintext;
@@ -121,7 +166,7 @@ void MainWindow::on_decryptbutton_clicked()
     char ciphertext[4096];
     char ciphertextb64[14096];
     char *p, *l;
-    int j=0, k=0, iterations=0;
+    int i=0, j=0, k=0, iterations=0;
     char c=0;
 
     memset(key,0,16);
@@ -163,6 +208,8 @@ void MainWindow::on_decryptbutton_clicked()
     memset(plainblock,0,16);
     memset(cipherblock,0,16);
 
+    QString output;
+    int flag=0; //flag if a \n is found, then exit from the cycle
     for(k=0; k<iterations; k++)
     {
         cipherblock[j]=*p++;
@@ -175,16 +222,51 @@ void MainWindow::on_decryptbutton_clicked()
             memcpy(plainwrite, plainblock, 16);
             plainwrite[17]='\0';
             //printf("%s", plainwrite);
-            ui->cleartextline->setText(ui->cleartextline->text()+QString((const char*)plainwrite));
+            for(i=0; i<17; i++)
+            {
+                if(plainwrite[i]=='\n' && flag==0)
+                {
+                    //memset(plainwrite+(i-1),0,18-i);
+                    ///plainwrite[i-2]='\0';
+                    plainwrite[i]='\0';
+                    flag=1;
+                }
+                if(flag==1)
+                    plainwrite[i]='\0';
+            }
+
+            output+=QString((const char*)plainwrite);
+            //ui->cleartextline->setText(ui->cleartextline->text()+QString((const char*)plainwrite));
+
 
 
             memset(plainwrite,0,18);
             memset(plainblock,0,16);
             memset(cipherblock,0,16);
+
+            //a \n was found... no more decoding
+            if(flag==1)break;
         }
         else j++;
     }
 
+    flag=0;
+    QString output_def;
+    for(i=0; i<output.length(); i++)
+    {
+        if(flag==0)
+        {
+            if(output.at(i)==']')
+                flag=1;
+        }
+        else
+        {
+            output_def.append(output.at(i));
+        }
+    }
+
+
+    ui->cleartextline->setText(output_def);
 
     ui->conversation->appendPlainText("Proxy: ");
     ui->conversation->appendPlainText(ui->cleartextline->text());
